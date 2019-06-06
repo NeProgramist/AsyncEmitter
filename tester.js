906d7201a1cac8209a2f18b7a5e51f5248458775
Zhenya <zasko180@gmail.com>
2019-06-06 16:09:41 +0300

    Fix

--- tester.js
+++ tester.js
@@ -3,7 +3,7 @@
 const aeObj = require('./AsyncEmitters/objects');
 const aeArr = require('./AsyncEmitters/arrays&objects');
 const aeHex = require('./AsyncEmitters/hash-tables');
-const aeSet = require('./AsyncEmitters/set&objects')
+const aeSet = require('./AsyncEmitters/set&objects');
 
 const COUNT = [10,100,1000,10000,100000];
 
@@ -14,7 +14,7 @@ async function speedChecker(ae, n) {
     const ee = new ae();
     cb.forEach((fn, index) => ee.on(index,fn));
 
-    console.time('Check')
+    console.time('Check');
     Promise.all(cb.map(async (fn, index) => {
       await ee.emit(index);
     }))
@@ -24,30 +24,30 @@ async function speedChecker(ae, n) {
 
 // Test
 
-console.log('-----aeObj-----')
+console.log('-----aeObj-----');
 COUNT.map(c => {
     console.log(c);
-    speedChecker(aeObj,c)
+    speedChecker(aeObj,c);
 });
-console.log('-----aeObj-----')
+console.log('-----aeObj-----');
 
-console.log('-----aeHash-----')
+console.log('-----aeHash-----');
 COUNT.map(c => {
     console.log(c);
-    speedChecker(aeHex,c)
+    speedChecker(aeHex,c);
 });
-console.log('-----aeHash-----')
+console.log('-----aeHash-----');
 
-console.log('-----aeArr-----')
+console.log('-----aeArr-----');
 COUNT.map(c => {
     console.log(c);
-    speedChecker(aeArr,c)
+    speedChecker(aeArr,c);
 });
-console.log('-----aeArr-----')
+console.log('-----aeArr-----');
 
-console.log('-----aeSet-----')
+console.log('-----aeSet-----');
 COUNT.map(c => {
     console.log(c);
-    speedChecker(aeSet,c)
+    speedChecker(aeSet,c);
 });
-console.log('-----aeSet-----')
+console.log('-----aeSet-----');
